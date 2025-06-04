519c60ec380667bf618bc08cfaade936be6b199d
Wallisson Russel <wallissom12@gmail.com>
2025-06-04 19:29:18 -0300

    Update app.py

--- app.py
+++ app.py
@@ -24,10 +24,8 @@ try:
         cred_dict = {
             "type": st.secrets["firebase_credentials"]["type"],
             "project_id": st.secrets["firebase_credentials"]["project_id"],
-            "private_key_id": st.secrets["firebase_credentials"]["private_key_id"],
             "private_key": st.secrets["firebase_credentials"]["private_key"].replace("\\n", "\n"),
             "client_email": st.secrets["firebase_credentials"]["client_email"],
-            "client_id": st.secrets["firebase_credentials"]["client_id"],
             "auth_uri": st.secrets["firebase_credentials"]["auth_uri"],
             "token_uri": st.secrets["firebase_credentials"]["token_uri"],
             "auth_provider_x509_cert_url": st.secrets["firebase_credentials"]["auth_provider_x509_cert_url"],
